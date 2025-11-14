a122234e0853e32ca6360c1ff60aeb654885a2c4
Vaibhav262610 <branda56235@gmail.com>
2025-11-14 23:04:41 +0530

    fix bug

--- src/server.js
+++ src/server.js
@@ -55,7 +55,7 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:4173',
     'https://temple-management-woad.vercel.app',
-    'https://temple-management-woad.vercel.app/'
+    'https://temple-management-woad.vercel.app/',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
